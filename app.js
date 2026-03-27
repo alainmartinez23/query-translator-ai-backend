fd7667c74649b283aa3be4626050a4c922de8cd3
alainmartinez23 <alainmartinezs23@gmail.com>
2026-03-27 18:43:18 +0100

    cors añadido

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const queryController = require('./controllers/queryController');
 const { queryRateLimiter } = require('./middlewares/rateLimiter');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.set('trust proxy', 1);
